d3475d8ccc9ceed2a533782761ec4f6db8129ff6
lmfalbertini1996 <109661090+lmfalbertini1996@users.noreply.github.com>
2023-05-22 18:47:12 +0200

    Update surveyed.py

--- pages/surveyed.py
+++ pages/surveyed.py
@@ -550,9 +550,8 @@ if which_mode == 'Entire Area':
                 clusters = ['ID:'+str(i) for i in clusters]
                 plot = go.Figure(data=[go.Pie(labels=clusters, values=values)])
                 st.plotly_chart(plot)
-    #key=14
-    detailed_table_survey = st.sidebar.selectbox('------ Detailed Table Surveys------', ['ON', 'OFF'], key=key, index=1)
     key=14
+    detailed_table_survey = st.sidebar.selectbox('------ Detailed Table Surveys------', ['ON', 'OFF'], key=key, index=1)
     if detailed_table_survey == "ON":
         "# Survey outcome"
 
@@ -565,9 +564,9 @@ if which_mode == 'Entire Area':
         file_gdf_for_table = file_gdf[survey_outcomes]
         st.dataframe(file_gdf_for_table)
 
-    #key = 15
-    detailed_table_vania = st.sidebar.selectbox('------ Detailed Table Vania------', ['ON', 'OFF'], key=key, index=1)
     key = 15
+    detailed_table_vania = st.sidebar.selectbox('------ Detailed Table Vania------', ['ON', 'OFF'], key=key, index=1)
+    #key = 15
     if detailed_table_vania == "ON":
         "# VANIA outcome"
 
